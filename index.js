571ec8cfd88a73370e34535e01b395f4a41035ee
Ahmed Sherif <ahmed.sherif.fci@gmail.com>
2019-04-02 14:32:23 +0200

    ready to push

---
+++ index.js
@@ -0,0 +1,122 @@
+// requirng modules
+var express = require("express");
+var socket = require("socket.io");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var path = require("path");
+
+// app setup
+var app = express();
+
+var port = process.env.PORT || 4001;
+var host = process.env.HOST || "localhost";
+
+// initiate server
+var server = app.listen(port, function () {
+    console.log(`Listening at http://${host}:${port}`);
+});
+
+// use static files in public folder
+app.use(express.static("public"));
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// use ejs template
+app.set("view engine","ejs");
+app.set("views","templates");
+app.set('views', path.join(__dirname, "views"));
+
+// connect to mongodb and create chat db if it doesn't exists
+mongoose.connect("mongodb://localhost:27017/chat",{ useNewUrlParser: true });
+
+var chatSchema = mongoose.Schema;
+
+var userSchema = new chatSchema({
+    Handle: String,
+    Password: String
+});
+
+var User = mongoose.model("user", userSchema);
+var db = mongoose.connection;
+
+db.on("error", function (err) {
+    console.log(err);
+
+});
+
+db.once("open", function () {
+    console.log("DB Connected");
+
+    app.get("/",(req,res)=>{
+        res.render("Login");
+    });
+
+    app.get("/Login",(req,res)=>{
+        res.render("Login");
+    });
+
+    app.get("/Register",(req,res)=>{
+        res.render("Register");
+    });
+
+    app.post("/Register",(req,res)=>{
+        var formData = req.body;
+        var newUser = User({
+            Handle:formData.handle,
+            Password:formData.password
+        });
+
+        newUser.save((err)=>{
+            if(err) throw err;
+
+            console.log("Person Saved !");
+        });
+
+        res.render("ChatRoom",{Handle:formData.handle});
+    });
+
+    app.post("/Login",function(req,res){
+        var formData = req.body;
+        // here I used count to check if the entered user is in the system
+        User.count({Handle:formData.handle,Password:formData.password},function(err,count){
+            if(count > 0){
+                res.render("ChatRoom",{Handle:formData.handle});
+            }
+            else{
+                res.render("Login",{Error:"Handle or/and Password is Incorrect"})
+            }
+        });
+    });
+
+    // socket setup
+    // socket requires a server as parameter,
+    // for that we registered the app.listen into server variable so we can use it here
+    var io = socket(server);
+
+    // listening to any socket connection happens to the server from the client side
+    io.on("connection", function (socket) {
+        // log the connection with client socket connection id , {changes with every refresh}
+        console.log(`Made Socket Connection ${socket.id}`);
+
+        // here we need to listen to the message sent by the client using the socket
+        // and reseve the object from the server in the parameter of the callback function
+        socket.on("chat", function (data) {
+            // io.sockets referes to all the sockets connected to the server
+            // so we emit the chat message to all the clients again
+            // and when receving the data we can put it on the screen to all the users
+            io.sockets.emit("chat", data);
+        });
+
+        // Handle typing event so it looks cool using broadcast
+        socket.on("typing", function (data) {
+            // socket.broadcast send the data on the emitted event to all other users
+            // so we emit the typing message
+            // and when receving the data we can put it on the screen to the other users
+            socket.broadcast.emit("typing", data);
+        });
+    });
+
+});
---
+++ public/js/chat.js
@@ -0,0 +1,34 @@
+// make socket connection
+// this will fire the connection event we made on the server
+var socket = io.connect("http://localhost:4001");
+
+// Emit events to the server
+$("#send").on("click",function(){
+    // emit message using the socket to the server with 'chat' as the message name
+    // and the second parameter is object that holds the message itself and the handle of the user who's writing it
+    socket.emit("chat",{
+        message:$("#message").val(),
+        handle:$("#handle").val()
+    });
+});
+
+$("#message").on("keypress",function () {
+    // emit message using the socket to the server with 'typing' as the message name
+    // so we can broadcast the typing div to the other users with handle name
+    socket.emit("typing",$("#handle").val());
+});
+
+// Listen for events happend on the socket
+socket.on("chat",function (data) {
+    $("#feedback").html("");
+    $("#output").append(`<p><strong>${data.handle}:</strong> ${data.message}</p>`);
+});
+
+socket.on("typing",function (data) {
+    $("#feedback").html(`<p><em>${data} is typing a message ...</em></p>`);
+});
+
+// Show error message on login page
+if($(".alert").text().toString().length > 1){
+    $(".alert").removeClass("d-none").addClass("d-block");
+}
