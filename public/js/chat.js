66df4dce7d115292b8dea3460b306b4609ef4b59
Ahmed Sherif <ahmed.sherif.fci@gmail.com>
2019-04-03 19:57:39 +0200

    making the master as localhost version

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,6 @@
 // make socket connection
 // this will fire the connection event we made on the server
-var socket = io.connect("https://nodejschatsdsocketwithdb.herokuapp.com");
+var socket = io.connect("https://localhost:4001");
 
 // Emit events to the server
 $("#send").on("click",function(){
